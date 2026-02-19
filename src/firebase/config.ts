1d7bc11c49c92ff7750509fa24b2e0734b22a45e
johnycampos <63217971+johnycampos@users.noreply.github.com>
2026-02-19 02:49:19 -0300

    Delete src/firebase/config.ts

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAXKjOpali05XGqtKgVfUA4SLQokJNTUa8",
-  authDomain: "studio-369324334-53462.firebaseapp.com",
-  projectId: "studio-369324334-53462",
-  storageBucket: "studio-369324334-53462.firebasestorage.app",
-  messagingSenderId: "322211540651",
-  appId: "1:322211540651:web:317df89df225435e0111ee"
-};
